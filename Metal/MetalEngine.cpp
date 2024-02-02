f0b1343f82038d6bc5ddea51b21133835d4817c4
Linx145 <linussoon@gmail.com>
2024-02-02 18:58:41 +0800

    Thing compiles on mac now

---
+++ Metal/MetalEngine.cpp
@@ -0,0 +1,8 @@
+//
+//  MetalEngine.cpp
+//  Astral.Canvas
+//
+//  Created by Sunhill Latisha on 31/1/24.
+//
+
+#include <stdio.h>
---
+++ include/Graphics/Metal/MetalEngine.hpp
@@ -0,0 +1,12 @@
+//
+//  MetalEngine.hpp
+//  Astral.Canvas
+//
+//  Created by Sunhill Latisha on 31/1/24.
+//
+
+#ifndef MetalEngine_h
+#define MetalEngine_h
+
+
+#endif /* MetalEngine_h */
