8a1ebebe2f6b55c104df2e172b43449de84400c2
Linx145 <linussoon@gmail.com>
2024-02-03 12:35:23 +0800

    Metalwork

---
+++ Test/TestContent/MetalShader.shaderobj.metal
@@ -0,0 +1,11 @@
+//
+//  MetalShader.shaderobj.metal
+//  AstralCanvasTest
+//
+//  Created by Sunhill Latisha on 3/2/24.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+
---
+++ include/Graphics/Metal/MetalEnumConverters.h
@@ -0,0 +1,12 @@
+//
+//  MetalEnumConverters.h
+//  Astral.Canvas
+//
+//  Created by Sunhill Latisha on 2/2/24.
+//
+
+#ifndef MetalEnumConverters_h
+#define MetalEnumConverters_h
+
+
+#endif /* MetalEnumConverters_h */
